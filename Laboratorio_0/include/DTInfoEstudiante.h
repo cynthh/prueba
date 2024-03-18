c4e5a6a07923486cb10b412cdcbc86f24ef04514
luki <lucasbenitez5047@gmail.com>
2024-03-18 20:27:29 -0300

    DTinfoEstudiante.h

---
+++ Laboratorio_0/include/DTInfoEstudiante.h
@@ -0,0 +1,11 @@
+#include <stl>
+#include <string>
+
+class DTInfoEstudiante{
+private:
+    int ci, identificadorInfo;
+    string NombreEst;
+public:
+    DTInfoEstudiante(int ci, int identificadorInfo, string NombreEst);
+};
+
