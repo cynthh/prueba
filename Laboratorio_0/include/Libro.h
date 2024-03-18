7c93f0e6bec41e86ecbf80010040fa9f7ceaaa32
Santi46M <santimendoza499@gmail.com>
2024-03-18 20:40:22 -0300

    libro.h

---
+++ Laboratorio_0/include/Libro.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <stl>
+#include <string>
+#include "Informacion.h"
+#include "DTFecha.h"
+#include "DTInfoEstudiante.H"
+using namespace std;
+
+class Libro
+{
+private:
+    string titulo, autores, resumen;
+
+public:
+    Libro(string titulo, string autores, string resumen);
+    string getTitulo(string titulo);
+    string getAutores(string autores);
+    string getResumen(string resumen);
+    void resumen(string resumen);
+    string toString(int id, *DTFecha, string titulo, string autores, string resumen);
+}
