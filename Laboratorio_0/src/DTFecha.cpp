f60eb3a90033ce0797723f5a276ef92954c2b301
Santi46M <santimendoza499@gmail.com>
2024-03-18 22:30:51 -0300

    includes

--- Laboratorio_0/src/DTFecha.cpp
+++ Laboratorio_0/src/DTFecha.cpp
@@ -1,17 +1,22 @@
 #include "DTFecha.h"
 
-DTFecha::DTFecha(int dia, int mes, int anio){
-    this->dia=dia;
-    this->mes=mes; 
+DTFecha::DTFecha(int dia, int mes, int anio)
+{
+    this->dia = dia;
+    this->mes = mes;
     this->anio = anio;
 }
-int DTFecha::compararDTFechas(DTFecha fecha1, DTFecha fecha2){
-	int res = 0;
-    if ((fecha1 -> anio != fecha2 -> anio) || (fecha1 -> mes != fecha2 -> mes) || (fecha1 -> dia != fecha2 -> dia)){
-        if ((fecha1 -> anio < fecha2 -> anio) || ((fecha1 -> anio == fecha2 -> anio) && (fecha1 -> mes < fecha2-> mes ))||
-        ((fecha1 -> anio == fecha2 -> anio) && (fecha1 -> mes == fecha2 -> mes) && (fecha1 -> dia < fecha2 -> dia))){
+int DTFecha::compararDTFechas(DTFecha fecha1, DTFecha fecha2)
+{
+    int res = 0;
+    if ((fecha1->anio != fecha2->anio) || (fecha1->mes != fecha2->mes) || (fecha1->dia != fecha2->dia))
+    {
+        if ((fecha1->anio < fecha2->anio) || ((fecha1->anio == fecha2->anio) && (fecha1->mes < fecha2->mes)) ||
+            ((fecha1->anio == fecha2->anio) && (fecha1->mes == fecha2->mes) && (fecha1->dia < fecha2->dia)))
+        {
             res = -1;
-        } else 
+        }
+        else
             res = 1;
     }
     return res;
--- Laboratorio_0/src/PaginaWeb.cpp
+++ Laboratorio_0/src/PaginaWeb.cpp
@@ -1,33 +1,38 @@
 #include "PaginaWeb.h"
 #include <stl>
 
-PaginaWeb()
-void setTitulo(string Titulo, PaginaWeb pagina){
-    this->Titulo = Titulo;
+PaginaWeb() void setTitulo(string Titulo, PaginaWeb pagina)
+{
+	this->Titulo = Titulo;
 }
 
-string getTitulo(PaginaWeb pagina){
-    return this->Titulo;
+string getTitulo(PaginaWeb pagina)
+{
+	return this->Titulo;
 }
 
-void setLink(string Titulo, PaginaWeb pagina){
-    this->Link = Titulo;
+void setLink(string Titulo, PaginaWeb pagina)
+{
+	this->Link = Titulo;
 }
 
-string getLink(PaginaWeb pagina){
-    return this->Link;
+string getLink(PaginaWeb pagina)
+{
+	return this->Link;
 }
 
-void setText(string Text, PaginaWeb pagina){
-    this->Text = Text;
+void setText(string Text, PaginaWeb pagina)
+{
+	this->Text = Text;
 }
 
-string getText(PaginaWeb pagina){
-    return this->Text;
+string getText(PaginaWeb pagina)
+{
+	return this->Text;
 }
 
-string toString(PaginaWeb pagina){
-    string res = "PáginaWeb: " + this->Identificador +", "+ this->Fecha +", "+ this->Titulo +", "+ this->Link +", "+ this->Text +".";
-    return res;
+string toString(PaginaWeb pagina)
+{
+	string res = "PáginaWeb: " + this->Identificador + ", " + this->Fecha + ", " + this->Titulo + ", " + this->Link + ", " + this->Text + ".";
+	return res;
 }
-
