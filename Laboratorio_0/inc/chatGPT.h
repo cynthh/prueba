cf26bcd5f2dd41a3d43d182727cf66726dc0b7eb
Santi46M <santimendoza499@gmail.com>
2024-03-26 18:24:55 -0300

    b

--- Laboratorio_0/inc/chatGPT.h
+++ Laboratorio_0/inc/chatGPT.h
@@ -1,8 +1,8 @@
 #ifndef chatGPT_H
 #define chatGPT_H
 
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 #include <string>
 #include <set>
 #include "DTFecha.h"
@@ -10,18 +10,19 @@
 #include "Informacion.h"
 using namespace std;
 
-class chatGPT : public Informacion{
+class chatGPT : public Informacion
+{
 private:
     string pregunta, respuesta;
-    //Estudiante *GuardadoPor_chatGpt;
+    // Estudiante *GuardadoPor_chatGpt;
 public:
-    chatGPT(int id, const DTFecha& fecha, string pregunta, string respuesta);
+    chatGPT(int id, const DTFecha &fecha, string pregunta, string respuesta);
     ~chatGPT();
     string getPregunta();
     string getRespuesta();
-    //void setPregunta(string);
-    //void setRespuesta(string);
+    // void setPregunta(string);
+    // void setRespuesta(string);
     string toString();
 };
 
-#endif 
+#endif
