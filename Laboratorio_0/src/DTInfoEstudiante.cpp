6581969a8f573dd8614a78fe38a0b191f43ba691
Santi46M <santimendoza499@gmail.com>
2024-03-25 22:17:49 -0300

    dtinfo

---
+++ Laboratorio_0/src/DTInfoEstudiante.cpp
@@ -0,0 +1,32 @@
+#ifndef DTINFOESTUDIANTE_H
+#define DTINFOESTUDIANTE_H
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <set>
+#include "DTFecha.h"
+#include "DTInfoEstudiante.h"
+#include "Informacion.h"
+using namespace std;
+class DTInfoEstudiante
+{
+private:
+	int ci, identificadorInfo;
+	string NombreEst;
+	*Informacion Arr[3];
+
+public:
+	DTInfoEstudiante(int ci, int identificadorInfo1, int identificadorInfo2, int identificadorInfo3, string NombreEst);
+	Informacion **Guardado(Informacion *info1, Informacion *info2, Informacion *info3);
+};
+
+// implementacion de las operaciones:
+Informacion **DTInfoEstudiante()::Guardado(Informacion *info1, Informacion *info2, Informacion *info3);
+{
+	Arr[0] = info1;
+	Arr[0] = info2;
+	Arr[0] = info3;
+	return Arr;
+}
+
+#endif
